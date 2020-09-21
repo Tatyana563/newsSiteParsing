95d284ab9ba76d206840ec8ed079d1adafc1ce6d
Tanya.Petrenko <nikolaev.english@gmail.com>
2020-09-21 17:56:53 +0300

    item added

--- src/main/java/com/example/reporter/Main.java
+++ src/main/java/com/example/reporter/Main.java
@@ -3,16 +3,15 @@ package com.example.reporter;
 import com.example.reporter.dto.ArticleDto;
 import com.example.reporter.entity.Article;
 import com.example.reporter.repository.ArticleRepository;
-import com.example.reporter.service.ArticleService;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-
 
 import java.io.IOException;
 import java.text.ParseException;
@@ -25,12 +24,13 @@ public class Main {
     private static final String WORLD_NEWS = URL + "/world-news";
     private static final String PAGE_PARAM_FORMAT = WORLD_NEWS + "?page=%d";
     private static final String DATE_FORMAT = "dd.MM.yyyy' - 'HH:mm";
-
+    @Value("${reporter.api.number-of-pages}")
+    private Integer NumberOfPages;
     @Autowired
     ArticleRepository articleRepository;
 
 
-//    @Scheduled(fixedRate = 30000000)
+    //    @Scheduled(fixedRate = 30000000)
     public void getElements() throws IOException {
         Document doc = Jsoup.connect("https://reporter-ua.com/world-news").get();
 
@@ -58,8 +58,8 @@ public class Main {
 
 
                 Elements itemLink = page.select(".e-news-item-title>a");
-                System.out.println(itemLink.get(j).attr("LINK: "+ "href"));
-                ArticleDto articleDto = new ArticleDto(heading,description,date);
+                System.out.println(itemLink.get(j).attr("LINK: " + "href"));
+                ArticleDto articleDto = new ArticleDto(heading, description, date);
 //                service.save(articleDto);
                 //  System.out.println("ref:"+ ref.get(j).attr("href"));
                 //     System.out.println("LINK:"+links.get(j).attr("href"));
@@ -74,18 +74,19 @@ public class Main {
     @Scheduled(fixedRate = 30000000)
     @Transactional
     public void getElements2() throws IOException, ParseException {
-        //TODO: move to app properties
-        int n = 10;
-        for(int i=0; i < n; i++) {
+        for (int i = 0; i < NumberOfPages; i++) {
             Document newsPage = Jsoup.connect(String.format(PAGE_PARAM_FORMAT, i)).get();
             processPage(newsPage);
         }
     }
 
-    private void processPage(Document page) throws ParseException {
+    private void processPage(Document page) throws ParseException, IOException {
         Elements articleElements = page.select("div.e-news > div.e-news-item");
         SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
         for (Element articleElement : articleElements) {
+            String description = null;
+            String text = null;
+            String source = null;
             String image = articleElement.selectFirst(".e-news-item-img img").attr("src");
             String heading = articleElement.selectFirst(".e-news-item-title a").text();
             String articleUrl = articleElement.selectFirst(".e-news-item-title a").absUrl("href");
@@ -94,16 +95,27 @@ public class Main {
             String shorDescription = articleElement.selectFirst("div.e-news-item-content > div.e-news-item-text").text();
 
             //TODO: Get and parse article page
+            Document articlePage = Jsoup.connect(articleUrl).get();
+            Elements singleArticle = articlePage.select(".field-items");
+            description = singleArticle.select(".field-item  strong").text();
+            text = singleArticle.select(".field-item p:not(:first-of-type)").text();
+            source = singleArticle.select(".field-item > p:nth-child(1) > strong:nth-child(1) > a:nth-child(1)").text();
+
+            Elements category = articlePage.select(".topmenu2");
+            String categ = category.select("li.active-trail:nth-child(1) > a:nth-child(1)").text();
 
             Article article = new Article();
+
             article.setDate(date);
             article.setHeading(heading);
             article.setLink(articleUrl);
             article.setShortDescription(shorDescription);
             article.setImageUrl(image);
+            article.setSource(source);
+            article.setDescription(description);
+            article.setText(text);
 
             articleRepository.save(article);
-
             System.out.println(article);
         }
     }
